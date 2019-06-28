2fc702cbbb994254686f1ea532e3bb76803aa672
Cool Fire Giant <coolfiregiant@gmail.com>
2019-06-28 10:30:50 -0300

    Update Program.cs

--- AutoForponto.Console/Program.cs
+++ AutoForponto.Console/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //Temporizador.Start();
-            AutoForponto.Model.Marcador.MarcarPonto("andre.carvalho", "Andre_01");
+            AutoForponto.Model.Marcador.MarcarPonto("meu.login", "M1nh453nh@");
             //new Model.Logger().Log("TESTE");
         }
     }
